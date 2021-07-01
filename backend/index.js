2daba4c5cb25af67bc87b068e91c730c66965c04
Aleksi <a12345@live.fi>
2021-07-01 23:28:21 +0300

    Created DB-connection
    - backend: Created connection to MySQL DB
    - Also refactored frontend App.js component to arrow function

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,19 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "reactnodecrud",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected to MySQL DB.");
+});
+
 app.listen(port, () => {
   console.log("App listening at http://localhost:${port]");
 });
